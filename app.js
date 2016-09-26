cb9968eb8ce7375de5182b78904083248026bd83
Steve Kluck <skluck@gmail.com>
2016-09-26 12:21:39 -0400

    Add js code for separate category/rating components

--- app.js
+++ app.js
@@ -1,22 +1,74 @@
+var segment = window.location.hash,
+    default_source = "http://kluck.engineering/skill-survey/blank-competencies.json";
+
+Vue.component('ratings', {
+  template: '#ratings',
+  props: ['ratings', 'unratings']
+})
+
+Vue.component('categories', {
+  template: '#categories',
+  props: ['categories']
+})
+
 var app = new Vue({
     el: '#evaluator',
 
     data: {
         last_message: "",
-        source: 'http://kluck.engineering/skill-survey/blank-competencies.json',
+        source:  segment.length > 0 ? segment.slice(1) : default_source,
+
         competencies: null,
         form: null,
-        ratings: [0, 1, 2, 3],
+
+        categories: [
+            {
+                "name": "Skill",
+                "description": "These competencies should be primarily scored based on the ability of the person to perform at a high level.",
+                "style": "green"
+            },
+            {
+                "name": "Knowledge",
+                "description": "These competencies should be primarily scored based on retained knowledge and expertise of the person.",
+                "style": "blue"
+            },
+            {
+                "name": "Behavior",
+                "description": "These competencies should be primarily scored based on ongoing behavior of the person.",
+                "style": "yellow"
+            }
+        ],
+        ratings: [
+            {
+                value: "0",
+                human: "Limited experience or knowledge. May have some familiarity but no hands-on or professional experience."
+            },
+            {
+                value: "1",
+                human: "Has some experience or knowledge. Can perform with limited oversight and guidance."
+            },
+            {
+                value: "2",
+                human: "Has production-level experience and deep hands-on knowledge. Can effectively share knowledge and assist others."
+            },
+            {
+                value: "3",
+                human: "Expert-level. Fully autonomous and can deliver consistently with an exceptional degree of quality."
+            },
+        ],
         unratings: [
             {
                 value: "IDK",
-                human: "I don't know",
-                tip: "Peer reviews only!"
+                human: "I don't know.",
+                tip: "Peer reviews only!",
+                extended: 'Does not have enough knowledge to judge or evaluate.',
+                warning: true
             },
             {
                 value: "N/A",
                 human: "Not applicable",
-                tip: "Competency is not relevant to person's role."
+                tip: "Not applicable or relevant to person's role or team.",
+                extended: "Not applicable or relevant to person's role or team."
             }
         ]
     },
@@ -31,26 +83,31 @@ var app = new Vue({
     methods: {
         fetchData: function () {
             this.last_message = "";
+            this.$http({ url: this.source, method: 'GET' })
+                .then(this.fetchSuccess, this.fetchError);
+        },
+        fetchSuccess: function(response) {
+            if (response.headers.get('Content-Type') !== 'application/json') {
+                this.last_message = "Blank survey data must be json.";
+                return;
+            }
 
-            var onSuccess = function (response) {
-                if (response.headers.get('Content-Type') !== 'application/json') {
-                    this.last_message = "Blank survey data must be json.";
-                    return;
+            var bySection = {};
+            response.data.forEach(function(entry) {
+                if (!bySection.hasOwnProperty(entry.section)) {
+                    bySection[entry.section] = [];
                 }
 
-                this.competencies = response.data;
-                this.form = {};
-                this.competencies.forEach(function(entry) {
-                    Vue.set(this.form, entry.id, entry.rating);
-                }, this);
-            },
-            onError = function(response) {
-                this.last_message = "Something terrible happened. Cannot load survey.";
-            };
+                bySection[entry.section].push(entry);
+            });
 
-            this.$http({ url: this.source, method: 'GET' })
-                .then(onSuccess, onError);
+            this.competencies = bySection;
+            this.buildFormData(response.data);
         },
+        fetchError: function(response) {
+            this.last_message = "Something terrible happened. Cannot load survey.";
+        },
+
         clearData: function () {
             this.last_message = "";
 
@@ -60,6 +117,13 @@ var app = new Vue({
 
         clearRating: function(competencyID) {
             Vue.set(this.form, competencyID, "");
+        },
+
+        buildFormData: function(competencies) {
+            this.form = {};
+            competencies.forEach(function(entry) {
+                Vue.set(this.form, entry.id, entry.rating);
+            }, this);
         },
 
         hoverRating: function(event) {

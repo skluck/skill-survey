ebdea7ae7fc3e96fe344c686ce223631c8b8135f
Steve Kluck <steve@kluck.engineering>
2017-11-04 19:32:33 -0400

    Nearly final editor
    Still need persistence and maybe some kind of validation against the
    json schema.

--- editor.js
+++ editor.js
@@ -7,7 +7,154 @@ var generate_uuid = function() {
     });
 };
 
-var clipboard = new Clipboard('.clipboard-btn');
+var clipboard = new Clipboard('.clipboard-btn'),
+    buildNewSurvey = function() {
+        return {
+            id: generate_uuid(),
+            type: '',
+            version: '1.0.0'
+        };
+    }
+    buildNewSection = function() {
+        return {
+            name: '',
+            competencies: []
+        };
+    },
+    buildNewCompetency = function() {
+        return {
+            id: '',
+            category: 'Skill',
+            competency: '',
+            examples: [
+                { rating: '0', example: ''},
+                { rating: '1', example: ''},
+                { rating: '2', example: ''},
+                { rating: '3', example: ''}
+            ],
+            rating: '',
+            comment: ''
+        };
+    },
+    survey_ratings = [
+        {
+            value: "0",
+            human: "Limited experience or knowledge. May have some familiarity but no hands-on or professional experience."
+        },
+        {
+            value: "1",
+            human: "Has some experience or knowledge. Can perform with limited oversight and guidance."
+        },
+        {
+            value: "2",
+            human: "Has production-level experience and deep hands-on knowledge. Can effectively share knowledge and assist others."
+        },
+        {
+            value: "3",
+            human: "Expert-level. Fully autonomous and can deliver consistently with an exceptional degree of quality."
+        },
+    ],
+    survey_unratings = [
+        {
+            value: "IDK",
+            human: "I don't know.",
+            tip: "Peer reviews only!",
+            extended: 'Does not have enough knowledge to judge or evaluate.',
+            warning: true
+        },
+        {
+            value: "N/A",
+            human: "Not applicable.",
+            tip: "Not applicable or relevant to person's role or team.",
+            extended: "Not applicable or relevant to person's role or team."
+        }
+    ],
+    survey_categories = [
+        {
+            name: "Skill",
+            description: "These competencies should be primarily scored based on the ability of the person to perform at a high level.",
+            style: "green"
+        },
+        {
+            name: "Knowledge",
+            description: "These competencies should be primarily scored based on retained knowledge and expertise of the person.",
+            style: "blue"
+        },
+        {
+            name: "Behavior",
+            description: "These competencies should be primarily scored based on ongoing behavior of the person.",
+            style: "yellow"
+        }
+    ];
+
+Vue.component('surveysection', {
+    template: '#ss-surveysection',
+    props: [
+        'parent_section',
+        'categories',
+        'ratings'
+    ],
+    data: function() {
+        return {
+            section: {}
+        }
+    },
+    created: function() {
+        this.section = this.parent_section;
+    },
+
+    methods: {
+        deleteCompetency: function(index) {
+            this.section.competencies.splice(index, 1);
+        },
+
+        addCompetency: function() {
+            this.section.competencies.push(buildNewCompetency());
+        }
+    }
+});
+
+Vue.component('competency', {
+    template: '#ss-competency',
+    props: [
+        'parent_competency',
+        'categories',
+        'ratings'
+    ],
+    data: function() {
+        return {
+            competency: {}
+        }
+    },
+    created: function() {
+        this.competency = this.parent_competency;
+    },
+    methods: {
+        resetExamples: function() {
+            this.competency.examples[0]['example'] = survey_ratings[0].human;
+            this.competency.examples[1]['example'] = survey_ratings[1].human;
+            this.competency.examples[2]['example'] = survey_ratings[2].human;
+            this.competency.examples[3]['example'] = survey_ratings[3].human;
+        }
+    }
+});
+
+Vue.component('example', {
+    template: '#ss-example',
+    props: [
+        'parent_example',
+        'score'
+    ],
+    data: function() {
+        return {
+            example: ""
+        }
+    },
+    created: function() {
+        this.example = this.parent_example;
+    }
+});
+
 
 Vue.component('message', {
     template: '#ss-message',
@@ -22,131 +169,9 @@ Vue.component('message', {
     }
 });
 
-Vue.component('surveysection', {
-    template: '#ss-section',
-    props: [
-        'section_id',
-        'section',
-        'categories',
-        'ratings',
-        'unratings'
-    ],
-    data: function() {
-        return {
-            new_competency: {
-                'id': '',
-                'category': '',
-                'competency': '',
-                'examples': {
-                    '0': '',
-                    '1': '',
-                    '2': '',
-                    '3': ''
-                },
-                'rating': '',
-                'comment': ''
-            }
-        };
-    },
-    methods: {
-        newCompetency: function() {
-            return {
-                'id': '',
-                'category': '',
-                'competency': '',
-                'examples': {
-                    '0': '',
-                    '1': '',
-                    '2': '',
-                    '3': ''
-                },
-                'rating': '',
-                'comment': ''
-            };
-        },
-        deleteSection: function() {
-            this.$emit('delete-section');
-        },
-        updateTitle: function(event) {
-            this.section.name = event.target.value;
-            this.$emit('input', this.section);
-        },
-
-        addCompetency: function() {
-            this.section.competencies.push(this.newCompetency());
-            this.$emit('input', this.section);
-        },
-
-        // triggered from nested update "set-competency" in competency
-        updateCompetency: function(competency_id, value) {
-            // console.log('triggered section update >>> from competency');
-            this.section.competencies[competency_id] = value;
-        }
-    }
-});
-
-Vue.component('competency', {
-    template: '#ss-competency',
-    props: [
-        'comp',
-        'categories',
-        'ratings'
-    ],
-
-    data: function() {
-        return {
-            changed_comp: {}
-        }
-    },
-    created: function() {
-        this.changed_comp = this.comp;
-    },
-
-    methods: {
-        updateID: function(event) {
-            this.changed_comp.id = event.target.value;
-            this.$emit('set-competency', this.changed_comp);
-        },
-        updateTitle: function(event) {
-            this.changed_comp.competency = event.target.value;
-            this.$emit('set-competency', this.changed_comp);
-        },
-        updateCategory: function(event) {
-            this.changed_comp.category = event.target.value;
-            this.$emit('set-competency', this.changed_comp);
-        },
-
-        // triggered from nested update "set-rating" in rating
-        updateRating: function(rating_score, value) {
-            // console.log('triggered competency update >>> from rating');
-            this.changed_comp.examples[rating_score] = value;
-            this.$emit('set-competency', this.changed_comp);
-        }
-    }
-});
-
-Vue.component('rating', {
-    template: '#ss-rating',
-    props: [
-        'score',
-        'example'
-    ],
-
-    data: function() {
-        return {
-            changed_rating: ''
-        }
-    },
-    created: function() {
-        this.changed_rating = this.example;
-    },
-
-    methods: {
-        emitRating: function(event) {
-            this.changed_rating = event.target.value;
-            this.$emit('set-rating', this.changed_rating);
-        }
-    }
+Vue.component('categories', {
+    template: '#ss-categories',
+    props: ['categories']
 });
 
 Vue.component('ratings', {
@@ -154,90 +179,17 @@ Vue.component('ratings', {
   props: ['ratings', 'unratings']
 });
 
-Vue.component('categories', {
-    template: '#ss-categories',
-    props: ['categories']
-});
-
-Vue.component('category', {
-    template: '#ss-category',
-    props: ['categories', 'selected_category'],
-    filters: {
-        abbrev: function(v) {
-            return v.slice(0, 1);
-        }
-    }
-});
-
-Vue.component('surveys', {
-    template: '#ss-surveys',
-    methods: {
-        loadBlankSurvey: function () {
-            this.$emit('load-new-survey');
-        }
-    }
-});
-
 var app = new Vue({
     el: '#ss-editor',
 
     data: {
-        last_message:      { message: "", error: false },
-        save_last_message: { message: "", error: false },
-
+        last_message: { message: "", error: false },
         survey: {},
         sections: [],
 
-        categories: [
-            {
-                "name": "Skill",
-                "description": "These competencies should be primarily scored based on the ability of the person to perform at a high level.",
-                "style": "green"
-            },
-            {
-                "name": "Knowledge",
-                "description": "These competencies should be primarily scored based on retained knowledge and expertise of the person.",
-                "style": "blue"
-            },
-            {
-                "name": "Behavior",
-                "description": "These competencies should be primarily scored based on ongoing behavior of the person.",
-                "style": "yellow"
-            }
-        ],
-        ratings: [
-            {
-                value: "0",
-                human: "Limited experience or knowledge. May have some familiarity but no hands-on or professional experience."
-            },
-            {
-                value: "1",
-                human: "Has some experience or knowledge. Can perform with limited oversight and guidance."
-            },
-            {
-                value: "2",
-                human: "Has production-level experience and deep hands-on knowledge. Can effectively share knowledge and assist others."
-            },
-            {
-                value: "3",
-                human: "Expert-level. Fully autonomous and can deliver consistently with an exceptional degree of quality."
-            },
-        ],
-        unratings: [
-            {
-                value: "IDK",
-                human: "I don't know.",
-                tip: "Peer reviews only!",
-                extended: 'Does not have enough knowledge to judge or evaluate.',
-                warning: true
-            },
-            {
-                value: "N/A",
-                human: "Not applicable.",
-                tip: "Not applicable or relevant to person's role or team.",
-                extended: "Not applicable or relevant to person's role or team."
-            }
-        ]
+        categories: survey_categories,
+        ratings: survey_ratings,
+        unratings: survey_unratings
     },
 
     computed: {
@@ -261,22 +213,14 @@ var app = new Vue({
 
     methods: {
         buildNewSurvey: function () {
-            this.survey = {
-                id: generate_uuid(),
-                type: '',
-                version: '1.0.0'
-            };
-
+            this.survey = buildNewSurvey();
             this.sections = [];
         },
         addSection: function() {
-            this.sections.push({
-                name: '',
-                competencies: []
-            });
+            this.sections.push(buildNewSection());
         },
-        deleteSection: function(section_id) {
-            this.sections.splice(section_id, 1)
+        deleteSection: function(index) {
+            this.sections.splice(index, 1);
         },
 
         clearSurvey: function () {
@@ -306,7 +250,18 @@ var app = new Vue({
 
                 for (comp_id in section.competencies) {
                     competency = section.competencies[comp_id];
-                    serialized[section.name]['competencies'][competency.id] = competency;
+                    serialized[section.name]['competencies'][competency.id] = {
+                        category: '' + competency.category,
+                        competency: competency.competency,
+                        examples: {
+                            '0': competency.examples[0].example,
+                            '1': competency.examples[1].example,
+                            '2': competency.examples[2].example,
+                            '3': competency.examples[3].example
+                        },
+                        rating: '',
+                        comment: ''
+                    };
                 }
             }
 
@@ -318,27 +273,10 @@ var app = new Vue({
                 error = true;
             }
 
-            this.save_last_message = {
+            this.last_message = {
                 message: message,
                 error: error
             };
-        },
-        setSaveBanner: function(message, error, shouldPop) {
-            error = (error === undefined) ? true : error;
-            shouldPop = (shouldPop === undefined) ? false : shouldPop;
-
-            this.save_last_message = {
-                message: message,
-                error: error
-            };
-
-            if (shouldPop && message.length > 0) {
-                var clearBanner = this.setSaveBanner;
-                setTimeout(function() { clearBanner(''); }, 3000);
-            }
-        },
-        setSaveBannerError: function(message, shouldPop) {
-            this.setSaveBanner(message, true, shouldPop);
         }
     }
 });
